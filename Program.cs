2308a418bd2c591bad48c3e9a614e1875226926a
Matt <halvorson34.mh@gmail.com>
2022-02-04 10:46:48 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BethanysPieShopHR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
