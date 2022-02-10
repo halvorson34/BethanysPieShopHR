4f623b502dc24d329e065f528683b29d33206dde
Matt <halvorson34.mh@gmail.com>
2022-02-10 17:00:40 -0600

    method practice

--- Program.cs
+++ Program.cs
@@ -6,73 +6,81 @@ namespace BethanysPieShopHR
     {
         static void Main(string[] args)
         {
+            //int monthlyWage = 1234;
+            //int months = 12, bonus = 1000;
+
+            //bool isActive = true;
+            //double rating = 99.25;
+
+            //double ratePerHour = 12.34;
+            //int numberOfHoursWorked = 165;
+
+            //long veryLongMonth = numberOfHoursWorked;
+
+
+            //double currentMonthWage = ratePerHour * numberOfHoursWorked;
+
+            //Console.WriteLine(currentMonthWage);
+
+            //ratePerHour += 3;
+
+            //if(currentMonthWage > 2000)
+            //{
+            //    Console.WriteLine("Top paid employee");
+            //}
+
+            //int numberOfEmployees = 15;
+            //numberOfEmployees--;
+
+            //int intMaxValue = int.MaxValue;
+            //int intMinValue = int.MinValue;
+
+            //char userSelection = 'a';
+            //char upperVersion = char.ToUpper(userSelection);
+
+            //DateTime hireDate = new DateTime(2021, 3, 28, 14, 30, 0);
+
+            //Console.WriteLine(hireDate);
+
+            //DateTime exitDate = new DateTime(2021, 12, 11);
+
+            //DateTime startDate = hireDate.AddDays(15);
+
+            //Console.WriteLine(startDate);
+
+            //string firstName = "Bethany";
+            //string lastName = "Smith";
+
+            //string fullName = firstName + " " + lastName;
+
+            //Console.WriteLine(fullName);
+
+            //string formatFullName = string.Format("Full name is {0} {1}.", firstName, lastName);
+
+            //Console.WriteLine(formatFullName);
+
+            //string interpolationFullName = $"Full name is {firstName} {lastName}.";
+
+            //Console.WriteLine(interpolationFullName);
+
+            //Console.WriteLine("Enter a wage:");
+            //string wage = Console.ReadLine();
+
+            //int wageValue = int.Parse(wage);
+
+            //Console.WriteLine($"Wage Value is: {wageValue}");
+
             int monthlyWage = 1234;
-            int months = 12, bonus = 1000;
+            int months = 12;
 
-            bool isActive = true;
-            double rating = 99.25;
-
-            double ratePerHour = 12.34;
-            int numberOfHoursWorked = 165;
-
-            long veryLongMonth = numberOfHoursWorked;
-           
-
-            double currentMonthWage = ratePerHour * numberOfHoursWorked;
-
-            Console.WriteLine(currentMonthWage);
-
-            ratePerHour += 3;
-
-            if(currentMonthWage > 2000)
-            {
-                Console.WriteLine("Top paid employee");
-            }
-
-            int numberOfEmployees = 15;
-            numberOfEmployees--;
-
-            int intMaxValue = int.MaxValue;
-            int intMinValue = int.MinValue;
-
-            char userSelection = 'a';
-            char upperVersion = char.ToUpper(userSelection);
-
-            DateTime hireDate = new DateTime(2021, 3, 28, 14, 30, 0);
-
-            Console.WriteLine(hireDate);
-
-            DateTime exitDate = new DateTime(2021, 12, 11);
-
-            DateTime startDate = hireDate.AddDays(15);
-
-            Console.WriteLine(startDate);
-
-            string firstName = "Bethany";
-            string lastName = "Smith";
-
-            string fullName = firstName + " " + lastName;
-
-            Console.WriteLine(fullName);
-
-            string formatFullName = string.Format("Full name is {0} {1}.", firstName, lastName);
-
-            Console.WriteLine(formatFullName);
-
-            string interpolationFullName = $"Full name is {firstName} {lastName}.";
-
-            Console.WriteLine(interpolationFullName);
-
-            Console.WriteLine("Enter a wage:");
-            string wage = Console.ReadLine();
-
-            int wageValue = int.Parse(wage);
-
-            Console.WriteLine($"Wage Value is: {wageValue}");
-
-
+            CalculateYearlyWage(monthlyWage, months);
 
             //Console.ReadLine();
+        }
+
+        public static void CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+        {
+            Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
         }
     }
 }
